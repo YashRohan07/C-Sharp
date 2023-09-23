ca78dea222871b0357ad21c5ebe515ecf30d3710
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-09-23 20:07:09 +0600

    Add files via upload

---
+++ 1. Basic/9. Switch Statement/Calculator Using Switch/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare variables for the first number, second number, and result.
+            double firstNumber, secondNumber, result = 0;
+
+            // Get the first number from the user.
+            Console.WriteLine("Enter the first number:");
+            firstNumber = Convert.ToDouble(Console.ReadLine());
+
+            // Get the second number from the user.
+            Console.WriteLine("Enter the second number:");
+            secondNumber = Convert.ToDouble(Console.ReadLine());
+
+            // Get the operator from the user.
+            char operatorSymbol;
+            Console.WriteLine("Enter the operator (+, -, *, /):");
+            operatorSymbol = (char)Console.Read();
+
+            // Switch on the operator symbol.
+            switch (operatorSymbol)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    break;
+
+                case '-':
+                    result = firstNumber - secondNumber;
+                    break;
+
+                case '*':
+                    result = firstNumber * secondNumber;
+                    break;
+
+                case '/':
+                    result = firstNumber / secondNumber;
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    break;
+
+                
+            }
+
+            // Display the result.
+            Console.WriteLine("The result is: " + result);
+
+            // Keep the console window open so that the user can see the result.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1. Basic/9. Switch Statement/Example-1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch; //Variable 'ch' is used to store character.
+            Console.WriteLine("Enter an alphabet:");
+            ch = Convert.ToChar(Console.ReadLine()); // Read a line of text from the console and convert it to a character, then store it in 'ch'.
+
+            switch (Char.ToLower(ch))
+            {
+                case 'a':
+                    Console.WriteLine("vowel");
+                    break;
+
+                case 'e':
+                    Console.WriteLine("vowel");
+                    break;
+
+                case 'i':
+                    Console.WriteLine("vowel");
+                    break;
+
+                case 'o':
+                    Console.WriteLine("vowel");
+                    break;
+
+                case 'u':
+                    Console.WriteLine("vowel");
+                    break;
+
+                default:
+                    Console.WriteLine("Not a vowel");
+                    break;
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
