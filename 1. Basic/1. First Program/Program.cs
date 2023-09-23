56fb25150af67859603006f364d2c1bb1e1b5568
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-09-23 20:03:19 +0600

    Add files via upload

---
+++ 1. Basic/1. First Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace First_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yash Rohan");
+
+            Console.Write("Virat");
+            Console.Write(" Rohit");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 1. Basic/5. Type Conversion Methods/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Type_Conversion_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            double b = 5.25;
+            bool c = true;
+
+            Console.WriteLine(Convert.ToString(a));  //Convert int to string
+            Console.WriteLine(Convert.ToDouble(a));  //Convert int to double
+            Console.WriteLine(Convert.ToInt32(b));   //Convert double to int
+            Console.WriteLine(Convert.ToString(c));  //Convert bool to string
+
+            Console.ReadLine();
+
+            /*
+            Output: 
+            10
+            10
+            5
+            True
+            */
+        }
+    }
+}
