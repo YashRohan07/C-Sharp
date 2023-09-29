e97af32884968db2c85802a915e219893fd94d98
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-09-29 18:38:08 +0600

    Update Program.cs

--- 1. Basic/5. Type Conversion Methods/Program.cs
+++ 1. Basic/5. Type Conversion Methods/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Type_Conversion_Methods
 {
@@ -9,21 +9,26 @@ namespace Type_Conversion_Methods
             int a = 10;
             double b = 5.25;
             bool c = true;
+            char d = 'A';
+            int e = 65;
 
-            Console.WriteLine(Convert.ToString(a));  //Convert int to string
-            Console.WriteLine(Convert.ToDouble(a));  //Convert int to double
-            Console.WriteLine(Convert.ToInt32(b));   //Convert double to int
-            Console.WriteLine(Convert.ToString(c));  //Convert bool to string
+            Console.WriteLine(Convert.ToString(a));       // Convert int to string
+            Console.WriteLine(Convert.ToDouble(a));       // Convert int to double
+            Console.WriteLine(Convert.ToInt32(b));        // Convert double to int
+            Console.WriteLine(Convert.ToString(c));       // Convert bool to string
+            Console.WriteLine(Convert.ToInt32(d));        // Convert char to int (ASCII value)
+            Console.WriteLine(Convert.ToChar(e));         // Convert int (ASCII value) to char
 
             Console.ReadLine();
-
-            /*
-            Output: 
-            10
-            10
-            5
-            True
-            */
         }
     }
 }
+/*
+Output:
+10
+10
+5
+True
+65
+A
+*/
