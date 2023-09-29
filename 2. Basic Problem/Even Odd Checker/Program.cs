466156698f128e03188ff93c37fa27083482e5d6
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-09-29 19:14:29 +0600

    Add files via upload

---
+++ 2. Basic Problem/Even Odd Checker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Even_Odd_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a digit: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
