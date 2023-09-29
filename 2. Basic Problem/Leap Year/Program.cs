16f8d51b1b9ab21c56894a47e5981483cc21fe7a
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-09-29 19:34:20 +0600

    Add files via upload

---
+++ 2. Basic Problem/Leap Year/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int year;
+        Console.Write("Enter a Year: ");
+        year = Convert.ToInt32(Console.ReadLine());
+
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+        {
+            Console.WriteLine("Leap Year");
+        }
+        else
+        {
+            Console.WriteLine("Not Leap Year");
+        }
+
+        Console.ReadLine();
+    }
+}
