706b602a326699beaaa17425ba5e360e954093d4
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-09-29 19:21:01 +0600

    Add files via upload

---
+++ 2. Basic Problem/Lab Task/Lab Task 1/Program.cs
@@ -0,0 +1,48 @@
+﻿/* Write a program which ask for a student score. Score is a number from 0 to 100. 
+Translate the score into grade like A,B...F. */
+using System;
+
+namespace GradeBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your marks: ");
+            int marks = Convert.ToInt32(Console.ReadLine());
+
+            char letterGrade;
+
+            if (marks >= 90 && marks <= 100)
+            {
+                letterGrade = 'A';
+            }
+            else if (marks >= 80 && marks < 90)
+            {
+                letterGrade = 'B';
+            }
+            else if (marks >= 70 && marks < 80)
+            {
+                letterGrade = 'C';
+            }
+            else if (marks >= 60 && marks < 70)
+            {
+                letterGrade = 'D';
+            }
+            else if (marks >= 0 && marks < 60)
+            {
+                letterGrade = 'F';
+            }
+            else
+            {
+                Console.WriteLine("Invalid marks");
+                return;
+            }
+
+            
+            Console.WriteLine("Your grade is:" +letterGrade);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2. Basic Problem/Lab Task/Lab Task 5/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Write a program that takes a number as input and prints its multiplication table up to 10. */
+
+using System;
+
+namespace Lab_Task_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Multiplication table for {n}:");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int product = n * i;
+                Console.WriteLine($"{n} * {i} = {product}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
