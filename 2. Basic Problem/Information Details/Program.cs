b3775b9b8f68c5de02e23d8a4c7177832d1e8c4e
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-09-29 19:56:48 +0600

    Add files via upload

---
+++ 2. Basic Problem/Information Details/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MyInfo_Updated
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, university, dept, id, email, bg;
+            double balance, cgpa;
+            int age, contact;
+
+            Console.WriteLine("Enter Your Info...");
+
+            Console.Write("Enter Your Name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter University Name: ");
+            university = Console.ReadLine();
+
+            Console.Write("Enter Dept: ");
+            dept = Console.ReadLine();
+
+            Console.Write("Enter Your ID: ");
+            id = Console.ReadLine();
+
+            Console.Write("Enter Your Age: ");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter CGPA: ");
+            cgpa = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter Email: ");
+            email = Console.ReadLine();
+
+            Console.Write("Enter Blood Group: ");
+            bg = Console.ReadLine();
+
+            Console.Write("Contact Number: ");
+            contact = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Your Net Worth: ");
+            balance = Convert.ToDouble(Console.ReadLine());
+
+            Console.ReadLine();
+        }
+    }
+}
