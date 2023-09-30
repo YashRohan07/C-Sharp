2172bd90a3971c12056ed512448d1cc549f293f6
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-01 01:43:58 +0600

    Add files via upload

---
+++ 4. String/Comparison/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = "Java";
+            string y = "Java";
+            string z = "Python";
+
+            Boolean result1 = x.Equals(y);
+            Console.WriteLine("x and y are equal: " +result1);
+
+            Boolean result2 = x.Equals(z);
+            Console.WriteLine("x and z are equal: " +result2);
+
+            Console.ReadLine();
+               
+
+        }
+    }
+}
