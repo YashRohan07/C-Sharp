3160612dc60990dd310e3452911fb962ecad4b53
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-09 13:23:01 +0600

    Add files via upload

---
+++ 7. OOP/4. Set Get/5. Example-2/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2
+{
+    class Student
+    {
+        private int stId;
+        private string stName;
+
+        public int ID///Property
+        {
+            set
+            {
+                if(value<=0)
+                {
+                    Console.WriteLine("Invalild ID");
+                }
+                else
+                {
+                    this.stId = value;
+                }
+                
+            }
+            get
+            {
+                return this.stId;
+            }
+        }
+        public string NAME///Property
+        {
+
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    Console.WriteLine("Please set/add your name.");
+                }
+                else
+                {
+                    this.stName = value;
+                }
+               
+                this.stName = value;
+            }
+            get
+            {
+                return this.stName;
+            }
+        }
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            
+            s.ID=55;
+            s.NAME="Yash";
+            Console.WriteLine(s.ID);
+            Console.WriteLine(s.NAME);
+     
+            Console.Read();
+        }
+    }
+}
---
+++ 7. OOP/4. Set Get/6. Example-3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2
+{
+    class Student
+    {
+        //AUTO IMPLEMENTED PROPERTY
+        public int ID { get; set; }
+        public string NAME { get; set; }
+
+        /*
+        public int ID { get; private set; }
+        public string NAME { get; private set; }
+        */
+
+        public Student(int i, string n)
+        {
+            ID = i;
+            NAME = n;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Student s = new Student(111, "Virat");
+
+            Console.WriteLine(s.ID);
+            Console.WriteLine(s.NAME);
+
+            Console.Read();
+        }
+    }
+}
