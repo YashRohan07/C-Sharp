0b4e7e447246ce3546afde6d904c71ceef4adbbb
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-10 00:50:40 +0600

    Add files via upload

---
+++ 7. OOP/Problem Solving/7. Book Class/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Book
+{
+    class Book
+    {
+        private string bookName;
+        private string bookAuthor;
+        private string bookId;
+        private string bookType;
+        private int bookCopies;
+        static int bookCounter;
+
+        public Book() { }
+        public Book(string name, string author, string id, string type, int copies)
+        {
+            bookName = name;
+            bookAuthor = author;
+            bookId = id;
+            bookType = type;
+            bookCopies = copies;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Book Name: {bookName}");
+            Console.WriteLine($"Author: {bookAuthor}");
+            Console.WriteLine($"Book ID: {bookId}");
+            Console.WriteLine($"Book Type: {bookType}");
+            Console.WriteLine($"Number of Copies: {bookCopies}");
+        }
+
+        public void AddBookCopy(int x)
+        {
+            bookCopies += x;
+            Console.WriteLine($"{x} copies added. Total copies now: {bookCopies}");
+        }
+
+        static void ShowTotalBookInfo()
+        {
+            Console.WriteLine($"Total books: {bookCounter}");
+        }
+
+        static void Main(string[] args)
+        {
+            Book myBook = new Book("The Catcher in the Rye", "J.D. Salinger", "123456", "Fiction", 5);
+
+            myBook.ShowInfo();
+            myBook.AddBookCopy(3);
+
+            bookCounter++;
+            ShowTotalBookInfo();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 7. OOP/Problem Solving/8. Triangle Class/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Triangle_Class
+{
+    class Triangle
+    {
+        private int x, y, z;
+
+        //Constructor
+        public Triangle(int side1, int side2, int side3)
+        {
+            x = side1;
+            y = side2;
+            z = side3;
+
+        }
+        public void TestTriangle()
+        {
+            if (x == y && y == z)
+            {
+                Console.WriteLine("Triangle is Equilateral");
+            }
+            else if (x == y || y == z || z == x)
+            {
+                Console.WriteLine("Triangle is Isosceles");
+            }
+            else
+            {
+                Console.WriteLine("Triangle is Scalene");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the length of side 1:");
+            int side1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the length of side 2:");
+            int side2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the length of side 3:");
+            int side3 = int.Parse(Console.ReadLine());
+
+            Triangle t = new Triangle(side1, side2, side3);
+
+            t.TestTriangle();
+            Console.ReadLine();
+        }
+
+    }
+}
