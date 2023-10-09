acb16fbb0ac858344627eb2293f95f65515f0f90
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-10 00:29:24 +0600

    Add files via upload

---
+++ 7. OOP/Problem Solving/6. Bank Account/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Account
+{
+    class Account
+    {
+        private string accName;
+        private string acId;
+        private int balance;
+
+        public Account() { }
+
+        public Account(string AccountName, string AccountId, int AccounBalance)
+        {
+            accName = AccountName;
+            acId = AccountId;
+            balance = AccounBalance;
+        }
+
+        public void Deposit(int amount)
+        {
+            balance += amount;
+            Console.WriteLine($"Deposited ${amount}, Then New Balance: ${balance}");
+        }
+
+        public void Withdraw(int amount)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrawn ${amount}, Then New Balance: ${balance}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance.");
+            }
+        }
+
+        public void Transfer(int amount, Account receiver)
+        {
+            if (amount <= balance)
+            {
+                balance -= amount;
+                receiver.Deposit(amount);
+                Console.WriteLine($"Transferred ${amount} , New Balance: ${balance}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance for Transfer.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Account senderAccount = new Account("Yash", "1", 5000);
+            Account receiverAccount = new Account("Rohan", "2", 3000);
+
+            senderAccount.Deposit(2000);
+            senderAccount.Withdraw(3000);
+            senderAccount.Transfer(1000, receiverAccount);
+
+            Console.ReadLine();
+        }
+    }
+}
