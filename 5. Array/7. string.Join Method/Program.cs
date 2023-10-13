7d99019a8be99cefb38ec7b7dca7591179ae6477
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-13 08:49:07 +0600

    Add files via upload

---
+++ 5. Array/7. string.Join Method/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+the string.Join method is used to concatenate the elements of the array into a single string,
+separating each element with a space.
+ */
+using System;
+class Program
+{
+    static void Main()
+    {
+        string[] words = { "Hello", "I", "am", "Yash", "Rohan"};
+        string result = string.Join(" ", words); // Join the array elements with a space separator
+
+        Console.WriteLine(result);
+        Console.ReadLine();
+    }
+}
