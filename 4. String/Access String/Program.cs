dbb831457106ff36b50b1b182567ea661db07378
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-13 23:24:15 +0600

    Update Program.cs

--- 4. String/Access String/Program.cs
+++ 4. String/Access String/Program.cs
@@ -1,17 +1,18 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Access_String
+namespace String
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
             string str = "Hello World";
 
+            //Accessing individual characters in the string
             Console.WriteLine(str[0]); // Output: H
             Console.WriteLine(str[1]); // Output: e
 
@@ -21,12 +22,37 @@ namespace Access_String
             Console.WriteLine(str.IndexOf("l")); // Output: 2
 
             // Substring() method
-            int charPos = str.IndexOf("W");    
+            int charPos = str.IndexOf("W");
             string lastName = str.Substring(charPos);
             Console.WriteLine(lastName);  // World
 
-            Console.ReadLine();
 
+            string Info = "Cecilia Hansen is 25 years old";
+            int start = Info.IndexOf("is") + 1;
+            Console.WriteLine(start);  //16
+
+            string substring = Info.Substring(start + 2, 2);
+            //string substring = Info.Substring(0, 6);  //0<=x<6
+            Console.WriteLine("Age is " + substring);
+            for (int count = 0; count <= Info.Length - 1; count++)
+            {
+                Console.Write(Info[count]);
+            }
+
+            Console.WriteLine();
+            Console.Read();
         }
     }
 }
+
+/*
+H
+e
+1
+2
+2
+World
+16
+Age is 25
+Cecilia Hansen is 25 years old
+*/
