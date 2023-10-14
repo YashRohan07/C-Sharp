e77c759d84d7343f9a65cd41ec5c7b9f098d1547
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-14 11:31:42 +0600

    Update Program.cs

--- 5. Array/1. Array Creation/Program.cs
+++ 5. Array/1. Array Creation/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Xml.Linq;
 
 namespace Array_Creation
 {
@@ -6,10 +7,10 @@ namespace Array_Creation
     {
         static void Main(string[] args)
         {
-            string[] car = new string[3]; // Create an array of 3 elements and add values later
             string[] bike = new string[3] { "Pulsar", "Yamaha", "Dominar" }; // Create an array of 3 elements and add values right away
             string[] color = new string[] { "Black", "White", "Red" }; // Create an array of 3 elements without specifying the size
             string[] teams = { "Aus", "Eng", "Ind" }; // Create an array of 3 elements, omitting the new keyword
+            string[] car = new string[3]; // Create an array of 3 elements and add values later
 
             // Add values to the "car" array
             car[0] = "Toyota";
@@ -21,6 +22,37 @@ namespace Array_Creation
             Console.WriteLine("bike: " + string.Join(", ", bike));
             Console.WriteLine("color: " + string.Join(", ", color));
             Console.WriteLine("teams: " + string.Join(", ", teams));
+            
+
+            // Print the contents of the "car" array
+            /* 
+            Console.WriteLine(car[0]);
+            Console.WriteLine(car[1]);
+            Console.WriteLine(car[2]);
+            */
+
+            /*
+            for(int i=0;i<3;i++)
+            {
+                Console.WriteLine(car[i]);
+            }
+            */
+            
+
+            /*
+            for (int i = 0; i < car.Length; i++)
+            {
+                Console.WriteLine(car[i]);
+            }
+            */
+
+            /*
+            foreach (string item in car)
+            {
+                Console.WriteLine(item);
+            }
+            */
+            
 
             Console.ReadLine();
         }
