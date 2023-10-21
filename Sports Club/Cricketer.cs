257ee638a4bf7c19ec810fb13c5ab8c311a0293d
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-21 13:49:17 +0600

    Add files via upload

---
+++ Sports Club/Cricketer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Cricketer : Player
+{
+    public int TotalRun { get; set; }
+    public int TotalWicket { get; set; }
+
+    public Cricketer(int id, string name, decimal salary, JoiningDate joiningDate, int totalRun, int totalWicket) : base(id, name, salary, joiningDate)
+    {
+        TotalRun = totalRun;
+        TotalWicket = totalWicket;
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Cricketer Information");
+        Console.WriteLine($"Id: {Id}");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Salary: {Salary}");
+        Console.WriteLine($"Joining Date: {JoiningDate}");
+        Console.WriteLine($"Total Run: {TotalRun}");
+        Console.WriteLine($"Total Wicket: {TotalWicket}");
+    }
+}
---
+++ Sports Club/Player.cs
@@ -0,0 +1,17 @@
+﻿public abstract class Player
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Salary { get; set; }
+    public JoiningDate JoiningDate { get; set; }
+
+    public Player(int id, string name, decimal salary, JoiningDate joiningDate)
+    {
+        Id = id;
+        Name = name;
+        Salary = salary;
+        JoiningDate = joiningDate;
+    }
+
+    public abstract void ShowInfo();
+}
