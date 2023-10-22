0683aea26752bcf09ee30e91582d82e23c0ebc28
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-22 21:44:49 +0600

    Add files via upload

---
+++ 8. Advance OOP/Inheritance/5. Hierarchical Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿//Multiple derived classes inherit from a single base class
+using System;
+
+// Base class
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+
+// First derived class
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Barking...");
+    }
+}
+
+// Second derived class
+class Cat : Animal
+{
+    public void Meow()
+    {
+        Console.WriteLine("Meowing...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        Cat cat = new Cat();
+
+        dog.Eat(); // Accessing the method from the base class
+        dog.Bark(); // Accessing the method from the first derived class
+
+        cat.Eat(); // Accessing the method from the base class
+        cat.Meow(); // Accessing the method from the second derived class
+
+        Console.ReadLine();
+    }
+}
---
+++ 8. Advance OOP/Inheritance/9. Base Keyword/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Base_Keyword
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("Animal Class");
+        }
+    }
+
+    class Dog : Animal
+    {
+        // Overridden method that calls the base class method
+        public override void eat()
+        {
+            // Calling the base class method using base keyword
+            base.eat();
+
+            Console.WriteLine("Dog Class");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an instance of the Dog class
+            Dog obj = new Dog();
+
+            // Calling the overridden method
+            obj.eat();
+            Console.ReadLine();
+        }
+    }
+}
