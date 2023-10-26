cc52fe0f9a1789d683b82a3c96f0a2400117542a
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-26 23:11:31 +0600

    Add files via upload

---
+++ 8. Advance OOP/1. Inheritance/Example-3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hierarchical_Inheritance
+{
+    class A
+    {
+        public int a;
+        public A()
+        {
+            a = 1;
+            Console.WriteLine("Constructor from class A");
+        }
+    }
+
+    class B : A
+    {
+        public int b;
+        public B()
+        {
+            b = 2;
+            Console.WriteLine("Constructor from class B");
+        }
+    }
+
+    class C : A
+    {
+        public int c;
+        public C()
+        {
+            c = 3;
+            Console.WriteLine("Constructor from class C");
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            C obj1 = new C();
+            Console.WriteLine("a = {0} ", obj1.a);
+            //Console.WriteLine("b = {0} ", obj1.b);
+            Console.WriteLine("c = {0} ", obj1.c);
+
+            Console.WriteLine();
+
+            B obj2 = new B();
+            Console.WriteLine("a = {0} ", obj2.a);
+            Console.WriteLine("b = {0} ", obj2.b);
+            //Console.WriteLine("c = {0} ", obj2.c);
+
+            Console.WriteLine();
+
+            A obj3 = new A();
+            Console.WriteLine("a = {0} ", obj3.a);
+            //Console.WriteLine("b = {0} ", obj3.b);
+            //Console.WriteLine("c = {0} ", obj3.c);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+/*
+ Output:
+
+Constructor from class A
+Constructor from class C
+a = 1
+c = 3
+
+Constructor from class A
+Constructor from class B
+a = 1
+b = 2
+
+Constructor from class A
+a = 1
+
+ */
---
+++ 8. Advance OOP/3. Abstraction/Example-1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Abstraction
+{
+    // Abstract class providing a blueprint for Animal
+    abstract class Animal
+    {
+        // Non-abstract method
+        public void Display1()
+        {
+            Console.WriteLine("Non abstract method");
+        }
+
+        // Abstract method to be implemented by derived classes
+        public abstract void Display2();
+
+        // Virtual method with a default implementation
+        public virtual void Display3()
+        {
+            Console.WriteLine("Virtual method");
+        }
+    }
+
+    class Dog : Animal
+    {
+        // Implementing the abstract method from the base class
+        public override void Display2()
+        {
+            Console.WriteLine("Abstract method");
+        }
+
+        // Overriding the virtual method from the base class
+        public override void Display3()
+        {
+            Console.WriteLine("Updated virtual method");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+
+            // Accessing the non-abstract method from the base class
+            dog.Display1(); // Outputs: Non abstract method
+
+            // Accessing the abstract method implemented in the derived class
+            dog.Display2(); // Outputs: Abstract method
+
+            // Accessing the overridden virtual method from the derived class
+            dog.Display3(); // Outputs: Updated virtual method
+
+            Console.ReadLine();
+        }
+    }
+}
