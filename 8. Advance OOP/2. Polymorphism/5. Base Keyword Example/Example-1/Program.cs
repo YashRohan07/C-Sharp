34338546499d9bb1d4a7c1c7e9ca04ffed31f8db
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-27 12:34:21 +0600

    Add files via upload

---
+++ 8. Advance OOP/2. Polymorphism/5. Base Keyword Example/Example-1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Base
+{
+    // Base class A
+    class A
+    {
+        public int a;
+
+        // Constructor for class A
+        public A(int x)
+        {
+            a = x;
+            Console.WriteLine("Constructor A(int): a = {0}", a);  //Constructor A(int): a = 0
+        }
+    }
+
+    // Derived class B from A
+    class B : A
+    {
+        public int b;
+
+        // Constructor for class B, calling the base class constructor with value 0
+        public B(int y) : base(0)
+        {
+            b = y;
+            Console.WriteLine("Constructor B(int): b = {0}", b);  //Constructor B(int): b = 4
+        }
+    }
+
+    // Derived class C from B
+    class C : B
+    {
+        public int c;
+
+        // Constructor for class C, calling the base class constructor with value 0
+        public C(int z) : base(0)
+        {
+            c = z;
+            Console.WriteLine("Constructor C(int): c = {0}", c); //Constructor C(int): c = 5
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object of class B
+            B objB = new B(4);
+            Console.WriteLine("------");  // ------
+
+            // Creating an object of class C
+            C objC = new C(5);
+
+            Console.ReadLine();
+        }
+    }
+}
+
+/* 
+Output:
+Constructor A(int): a = 0
+Constructor B(int): b = 4
+------
+Constructor A(int): a = 0
+Constructor B(int): b = 0
+Constructor C(int): c = 5
+*/
---
+++ 8. Advance OOP/2. Polymorphism/5. Base Keyword Example/Example-2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+// Base class Person
+public class Person
+{
+    protected string id = "444";
+    protected string name = "Yash Rohan";
+    public virtual void GetInfo()
+    {
+        Console.WriteLine("Name: {0}", name); 
+        Console.WriteLine("ID: {0}", id); 
+    }
+}
+
+// Derived class Employee from Person
+class Employee : Person
+{
+    public string id = "ABC567EFG"; // Shadowing the id field
+
+    // Override the GetInfo method from the base class
+    public override void GetInfo()
+    {
+        // Call the base class GetInfo method
+        base.GetInfo();
+
+        Console.WriteLine("Employee ID: {0}", base.id); // Print base class's id  (Employee ID: 444)
+        Console.WriteLine("Employee ID: {0}", id); // Print derived class's id    (Employee ID: ABC567EFG)
+        Console.WriteLine("Employee ID: {0}", this.id); // Print current instance's id  (Employee ID: ABC567EFG)
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Employee E = new Employee(); 
+        E.GetInfo(); // Call the GetInfo method of Employee
+
+        Console.ReadLine();
+    }
+}
+
+/*
+ 
+Output:
+Name: Yash Rohan
+ID: 444
+Employee ID: 444
+Employee ID: ABC567EFG
+Employee ID: ABC567EFG
+
+ */
