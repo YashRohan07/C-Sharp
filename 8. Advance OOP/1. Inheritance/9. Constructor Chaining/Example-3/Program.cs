8ba263f3400cb79e49242865ce1c28202fc87c63
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-10-28 19:32:37 +0600

    Update Program.cs

--- 8. Advance OOP/1. Inheritance/9. Constructor Chaining/Example-3/Program.cs
+++ 8. Advance OOP/1. Inheritance/9. Constructor Chaining/Example-3/Program.cs
@@ -62,7 +62,29 @@ namespace ConstructorChainingExample
 }
 
 /*
- Output:
+
+1. **User user1 = new User();**
+   - The `user1` object is created using the default constructor of the `User` class. It prints "Hi! " to the console.
+
+2. **User user2 = new User("Hey there!");**
+   - The `user2` object is created using the parameterized constructor of the `User` class. 
+   The `this` keyword is used to chain the current constructor to the default constructor, then prints "Hi! Hey there!" 
+
+3. **Student student1 = new Student();**
+   - The `student1` object is created using the default constructor of the `Student` class, 
+   which uses the `base` keyword to call the base class constructor with the parameter "Welcome to ". It prints "Hi! Welcome to CS Department. "
+
+4. **Student student2 = new Student("Proud Student");**
+   - The `student2` object is created using the parameterized constructor of the `Student` class.
+   The `base` keyword in the constructor calls the base class constructor with the parameter "Welcome ", and "Proud Student" is printed to the console.
+
+5. **Student student3 = new Student("Welcome ", "to AIUB");**
+   - The `student3` object is created using the parameterized constructor of the `Student` class.
+   The `base` keyword in the constructor concatenates the `status` and `department` strings and passes the concatenated string as a parameter to the constructor of the base class.
+   This results in "Hi! Welcome to AIUB - Enjoy your studies!" being printed to the console.
+   The `base` keyword here is used for constructor chaining, and it connects the derived class to the base class constructors by passing the necessary parameters.
+   
+Output:
 Hi!
 Hi! Hey there!
 Hi! Welcome to CS Department.
