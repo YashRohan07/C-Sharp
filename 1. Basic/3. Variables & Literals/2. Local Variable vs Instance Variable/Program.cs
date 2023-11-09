e46c2ccaa530ed314e096f1fc47f4e9484bfc6c8
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-11-09 11:35:08 +0600

    Add files via upload

---
+++ 1. Basic/3. Variables & Literals/2. Local Variable vs Instance Variable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class ExampleClass
+{
+    // Instance variable
+    private int instanceVariable = 10;
+
+    public void ExampleMethod()
+    {
+        // Local variable
+        int localVariable = 5;
+
+        Console.WriteLine("Instance Variable: " + instanceVariable);
+        Console.WriteLine("Local Variable: " + localVariable);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ExampleClass obj = new ExampleClass();
+        obj.ExampleMethod();
+
+        Console.ReadLine();
+    }
+}
+
+/*
+The instance variable (instanceVariable) is accessible throughout the entire ExampleClass. 
+The local variable (localVariable) is accessible only within the scope of the ExampleMethod method.
+ */
