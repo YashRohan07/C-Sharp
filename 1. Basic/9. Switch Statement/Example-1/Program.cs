e836bc8873b03ccb121407fa2c6cd1158ab22978
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-11-09 12:49:57 +0600

    Update Program.cs

--- 1. Basic/9. Switch Statement/Example-1/Program.cs
+++ 1. Basic/9. Switch Statement/Example-1/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Example_1
 {
@@ -10,28 +6,15 @@ namespace Example_1
     {
         static void Main(string[] args)
         {
-            char ch; //Variable 'ch' is used to store character.
             Console.WriteLine("Enter an alphabet:");
-            ch = Convert.ToChar(Console.ReadLine()); // Read a line of text from the console and convert it to a character, then store it in 'ch'.
+            char ch = Convert.ToChar(Console.ReadLine()); // Read a line of text from the console and convert it to a character, then store it in 'ch'.
 
             switch (Char.ToLower(ch))
             {
                 case 'a':
-                    Console.WriteLine("vowel");
-                    break;
-
                 case 'e':
-                    Console.WriteLine("vowel");
-                    break;
-
                 case 'i':
-                    Console.WriteLine("vowel");
-                    break;
-
                 case 'o':
-                    Console.WriteLine("vowel");
-                    break;
-
                 case 'u':
                     Console.WriteLine("vowel");
                     break;
@@ -39,7 +22,6 @@ namespace Example_1
                 default:
                     Console.WriteLine("Not a vowel");
                     break;
-
             }
 
             Console.ReadLine();
