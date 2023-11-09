809d5c84c5bebc3f35695cbb3187d82b2733a95a
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-11-09 12:30:39 +0600

    Update Program.cs

--- 1. Basic/7. Operators/Ternary Operator/Ternary Operator/Program.cs
+++ 1. Basic/7. Operators/Ternary Operator/Ternary Operator/Program.cs
@@ -21,3 +21,12 @@ namespace Ternary_Operator
         }
     }
 }
+
+/*
+result = (num % 2 == 0) ? "Even Number" : "Odd Number"; - It consists of three parts:
+
+1. (num % 2 == 0) is the condition. It checks whether the remainder of "num" divided by 2 is equal to 0, which determines whether the number is even.
+2.  ? "Even Number" is the expression to be executed if the condition is true.
+3.  : "Odd Number" is the expression to be executed if the condition is false.
+
+*/
