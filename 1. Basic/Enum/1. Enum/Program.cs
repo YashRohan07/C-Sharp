e7cb6efb90b86d5a6f03854d2e5306a94bbda3ee
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-11-09 12:58:53 +0600

    Update Program.cs

--- 1. Basic/Enum/1. Enum/Program.cs
+++ 1. Basic/Enum/1. Enum/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ENUM
 {
@@ -18,24 +18,24 @@ namespace ENUM
         static void Main(string[] args)
         {
             // Accessing the enum items with the dot syntax
-            Console.WriteLine(WeekDays.Monday);
-            Console.WriteLine(WeekDays.Tuesday);
-            Console.WriteLine(WeekDays.Wednesday);
-            Console.WriteLine(WeekDays.Thursday);
-            Console.WriteLine(WeekDays.Friday);
-            Console.WriteLine(WeekDays.Saturday);
-            Console.WriteLine(WeekDays.Sunday);
+            Console.WriteLine(WeekDays.Monday); //Monday
+            Console.WriteLine(WeekDays.Tuesday); //Tuesday
+            Console.WriteLine(WeekDays.Wednesday); //Wednesday
+            Console.WriteLine(WeekDays.Thursday); //Thursday
+            Console.WriteLine(WeekDays.Friday); //Friday
+            Console.WriteLine(WeekDays.Saturday); //Saturday
+            Console.WriteLine(WeekDays.Sunday); //Sunday
 
             Console.WriteLine("==========================");
 
             // Accessing the integer value
-            Console.WriteLine((int)WeekDays.Monday);
-            Console.WriteLine((int)WeekDays.Tuesday);
-            Console.WriteLine((int)WeekDays.Wednesday);
-            Console.WriteLine((int)WeekDays.Thursday);
-            Console.WriteLine((int)WeekDays.Friday);
-            Console.WriteLine((int)WeekDays.Saturday);
-            Console.WriteLine((int)WeekDays.Sunday);
+            Console.WriteLine((int)WeekDays.Monday); // Output: 0
+            Console.WriteLine((int)WeekDays.Tuesday); // Output: 1
+            Console.WriteLine((int)WeekDays.Wednesday); // Output: 5
+            Console.WriteLine((int)WeekDays.Thursday); // Output: 6
+            Console.WriteLine((int)WeekDays.Friday); // Output: 7
+            Console.WriteLine((int)WeekDays.Saturday); // Output: 8
+            Console.WriteLine((int)WeekDays.Sunday); // Output: 9
 
             Console.ReadLine();
         }
