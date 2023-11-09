00f2ac9bec7c0d5323c10e574b8535f85ae96b9e
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-11-09 12:38:16 +0600

    Update Program.cs

--- 1. Basic/8. C# Math/Program.cs
+++ 1. Basic/8. C# Math/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace C__Math
 {
@@ -6,11 +6,12 @@ namespace C__Math
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Math.Max(5,10));  //Returns the highest value of x and y
-            Console.WriteLine(Math.Min(5, 10)); //Returns the lowest value of x and y
-            Console.WriteLine(Math.Sqrt(25));   //Returns the square root of x
-            Console.WriteLine(Math.Abs(-10));   //Returns the absolute positive value of x
-            Console.WriteLine(Math.Round(9.99));  //Rounds a number to the nearest whole number
+            Console.WriteLine(Math.Max(5, 10));  // Returns the highest value of x and y.  Output: 10
+            Console.WriteLine(Math.Min(5, 10)); // Returns the lowest value of x and y.  Output: 5
+            Console.WriteLine(Math.Sqrt(25));   // Returns the square root of x.  Output: 5
+            Console.WriteLine(Math.Abs(-10));   // Returns the absolute positive value of x.  Output: 10
+            Console.WriteLine(Math.Round(9.99));  // Rounds a number to the nearest whole number.  Output: 10
+            Console.WriteLine(Math.Pow(2, 3)); // Returns the value of the first argument raised to the power of the second argument.  Output: 8
 
             Console.ReadLine();
         }
