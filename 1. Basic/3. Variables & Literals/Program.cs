2e3efcf85f702e68709e3ce7529714d03f9b4141
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-11-09 11:28:11 +0600

    Update Program.cs

--- 1. Basic/3. Variables & Literals/Program.cs
+++ 1. Basic/3. Variables & Literals/Program.cs
@@ -9,12 +9,17 @@ namespace Variables___Literals
             int value = 5;
 
             //Variable
-            Console.WriteLine(value);
+            Console.WriteLine(value);  //5
 
             //Literal
-            Console.WriteLine(50.05);
+            Console.WriteLine(50.05);  //50.05
 
             Console.ReadLine();
         }
     }
 }
+
+/*
+Console.WriteLine(value)   This line outputs the value of the variable value to the console. it will print the value 5.
+Console.WriteLine(50.05)   This line directly prints the literal value 50.05 to the console. A literal is a fixed value that is written directly into the source code.
+*/
