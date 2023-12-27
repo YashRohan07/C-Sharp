99c100aa344722af96e2a2d7e64ced678c3f992e
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2023-12-27 08:49:40 +0600

    Add files via upload

---
+++ Projects/Employee Management System/Employee/Main.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Employee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("John", "30");
+
+            PermanentEmployee permanentEmployee = new PermanentEmployee("Smith", "35", 50000);
+
+            ContractEmployee contractEmployee = new ContractEmployee("Johnson", "28", 30000);
+
+            Console.WriteLine("Employee: {0}, Age: {1}", employee.Name, employee.Age);
+            Console.WriteLine("Permanent Employee: {0}, Age: {1}, Salary: {2}", permanentEmployee.Name, permanentEmployee.Age, permanentEmployee.Salary);
+            Console.WriteLine("Contract Employee: {0}, Age: {1}, Salary: {2}", contractEmployee.Name, contractEmployee.Age, contractEmployee.Salary);
+
+            Console.ReadLine();  
+        }
+    }
+}
---
+++ Projects/Employee Management System/Employee/Permanent Class.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Employee
+{
+    class PermanentEmployee : Employee
+    {
+        private int salary;
+
+        public PermanentEmployee() { }
+        public PermanentEmployee(string name, string age, int salary) : base(name, age)
+        {
+            this.salary = salary;
+        }
+
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+    }
+}
