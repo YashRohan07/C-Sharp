6ead6dacd7e2551a97889969782ab4237885dcfc
Yash Rohan <111037103+YashRohan07@users.noreply.github.com>
2024-01-08 12:25:09 +0600

    Add files via upload

---
+++ Lab Task/Sports Club Management System/Sports Club/Footballer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Footballer : Player
+{
+    public int TotalGoal { get; set; }
+    public int TotalAssist { get; set; }
+
+    public Footballer(int id, string name, decimal salary, JoiningDate joiningDate, int totalGoal, int totalAssist) : base(id, name, salary, joiningDate)
+    {
+        TotalGoal = totalGoal;
+        TotalAssist = totalAssist;
+    }
+
+    public override void ShowInfo()
+    {
+        Console.WriteLine($"Footballer Information");
+        Console.WriteLine($"Id: {Id}");
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Salary: {Salary}");
+        Console.WriteLine($"Joining Date: {JoiningDate}");
+        Console.WriteLine($"Total Goal: {TotalGoal}");
+        Console.WriteLine($"Total Assist: {TotalAssist}");
+    }
+}
